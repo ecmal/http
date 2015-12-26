af915d3a23811413bb9ed7ccff9866e78748c654
Sergey Mamyan <sergey.mamyan@gmail.com>
2015-12-27 01:12:07 +0400

    http library ready

--- src/test/server.ts
+++ src/test/server.ts
@@ -1,11 +1,16 @@
-import {Server} from 'http/server';
+import {Server} from '../server';
+import {Rest} from '../rest';
 
-import {Rest} from 'http/rest';
+import '../handlers/files';
+import '../handlers/rest';
+
 
 @Rest('/hello')
 class HelloResource {
+
     private query:any;
     private headers:any;
+
     get(){
         return {
             resource : 'Hello',
@@ -15,10 +20,15 @@ class HelloResource {
     }
 }
 
-new Server({
+var server = new Server({
     host : '0.0.0.0',
     port : 3000,
     rest : {
         path : '/api'
+    },
+    files:{
+        path : './web'
     }
 }).start();
+
+
