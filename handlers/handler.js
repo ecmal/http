b4e3b278677df4550ecc1bbb9b6b41e20ce63702
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-05-19 10:26:32 +0400

    Move to TypeScript 1.8

---
+++ handlers/handler.js
@@ -0,0 +1,21 @@
+system.register("http/handlers/handler", [], function(system,module) {
+    var Handler = (function (__super) {
+        Handler.configure = function (server, config) {
+            this.server = server;
+            this.config = config;
+            return this;
+        };
+        return Handler;
+        function Handler() {
+        }
+    })();
+    module.define('class', Handler);
+    module.export("Handler", Handler);
+    return {
+        setters:[],
+        execute: function() {
+            Handler = module.init(Handler);
+        }
+    }
+});
+//# sourceMappingURL=handler.js.map
